67181969f89b4e8fee4e873ac2105a1afea9ded0
Bek1zo <bekizo@icloud.com>
2024-07-26 12:43:21 +0300

    [+] burger menu for small display

--- components/Header/Delivery.vue
+++ components/Header/Delivery.vue
@@ -5,17 +5,17 @@ const { width } = useWindowSize()
 </script>
 
 <template>
-  <div v-if="width > 768" class="flex justify-center items-center font-mono gap-4 h-12">
-    <div class="text-white cursor-pointer bg-one rounded-full w-full flex justify-center items-center h-full">
-      <div>
+  <div class="flex justify-center items-center font-mono h-12 show w-96 md:visible invisible">
+    <div class="text-white cursor-pointer bg-one rounded-full w-full flex justify-center items-center h-full p-2">
+      <div class="flex items-center justify-center">
         <img src="/assets/pin.svg" class="h-6 w-12 flex justify-center items-center m-auto" alt="Pin icon"/>
       </div>
-      <div class="flex items-center justify-center">
-        Укажите адрес
+      <div class="flex items-center justify-center md:text-lg text-sm">
+        Адрес доставки
       </div>
     </div>
   </div>
-  <div v-else>
+  <div>
 
   </div>
 </template>
--- components/Header/Menu.vue
+++ components/Header/Menu.vue
@@ -4,6 +4,12 @@ import { useWindowSize } from '@vueuse/core'
 
 const { width } = useWindowSize()
 
+const rightSideMenu = ref(false)
+
+const clickRightSideMenu = () => {
+  rightSideMenu.value = !rightSideMenu.value
+}
+
 </script>
 
 <template>
@@ -15,12 +21,27 @@ const { width } = useWindowSize()
       <li class="cursor-pointer hover:text-one transition-colors delay-100">Контакты</li>
       <li class="cursor-pointer hover:text-one transition-colors delay-100">Вход</li>
     </ul>
-    <div v-else class="pt-4 pl-2">
+    <div v-else @click="clickRightSideMenu()" class="pt-4 pl-2">
       <svg xmlns="http://www.w3.org/2000/svg" class="m-auto w-8 h-8" viewBox="0 0 50 50">
         <path d="M 0 7.5 L 0 12.5 L 50 12.5 L 50 7.5 Z M 0 22.5 L 0 27.5 L 50 27.5 L 50 22.5 Z M 0 37.5 L 0 42.5 L 50 42.5 L 50 37.5 Z"></path>
       </svg>
     </div>
   </div>
+
+  <Transition>
+  <div v-if="rightSideMenu" class="absolute h-full w-full top-0 right-0 bg-one">
+    <div @click="clickRightSideMenu()" class="cursor-pointer mr-10 text-4xl mt-5 flex justify-end">
+      ×
+    </div>
+      <ul class="flex flex-col gap-4 justify-center items-center mt-10 font-robot-slab">
+        <li class="cursor-pointer hover:text-white transition-colors delay-100">Меню</li>
+        <li class="cursor-pointer hover:text-white transition-colors delay-100">Акции</li>
+        <li class="cursor-pointer hover:text-white transition-colors delay-100">Бонусы</li>
+        <li class="cursor-pointer hover:text-white transition-colors delay-100">Контакты</li>
+        <li class="cursor-pointer hover:text-white transition-colors delay-100">Вход</li>
+      </ul>
+  </div>
+  </Transition>
 </template>
 
 <style scoped>
